094910251df79502d825414a0513c2794a01840b
guapipopo <726551667@qq.com>
2024-09-06 01:25:19 +0800

    新增菜品代码开发 This reverts commit 667a3500

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,10 +1,8 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
-import com.sky.annotation.AutoFill;
 import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
-import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -23,7 +21,6 @@ public interface EmployeeMapper {
      * 新增员工
      * @param employee
      */
-    @AutoFill(OperationType.INSERT)
     @Insert("insert into employee (name, username, password, phone, sex, id_number, create_time, update_time, create_user, update_user, status)" +
     "values (#{name},#{username}, #{password}, #{phone}, #{sex},#{ idNumber}, #{createTime}, #{updateTime}, #{createUser}, #{updateUser}, #{status})")
     void save(Employee employee);
@@ -38,7 +35,6 @@ public interface EmployeeMapper {
      * 根据主键动态修改员工属性
      * @param employee
      */
-    @AutoFill(OperationType.UPDATE)
     void update(Employee employee);
     /**
      * 根据id查询员工信息

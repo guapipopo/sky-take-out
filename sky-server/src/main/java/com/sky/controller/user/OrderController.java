8bc7471fa25891fa75262a6f9bbae0d547234213
guapipopo <726551667@qq.com>
2024-09-16 14:15:30 +0800

    用户下单代码开发

---
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -0,0 +1,34 @@
+package com.sky.controller.user;
+
+import com.sky.dto.OrdersDTO;
+import com.sky.dto.OrdersSubmitDTO;
+import com.sky.result.Result;
+import com.sky.service.OrderService;
+import com.sky.vo.OrderSubmitVO;
+import com.sky.vo.OrderVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("userOrderController")
+@RequestMapping("user/order")
+@Api(tags="c端-订单管理相关接口")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    /**
+     *  提交订单
+     * @param ordersSubmitDTO
+     * @return
+     */
+    @PostMapping("/submit")
+    public Result<OrderSubmitVO> submit(@RequestBody OrdersSubmitDTO ordersSubmitDTO){
+        return Result.success(orderService.submit(ordersSubmitDTO));
+    }
+}
